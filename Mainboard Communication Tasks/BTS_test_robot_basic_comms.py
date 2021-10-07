14abc06d77ef4f4236575a2f582fdb99e3a9cbb6
mbz <30321314+mbz4@users.noreply.github.com>
2021-10-08 01:21:53 +0300

    Update BTS_test_robot_basic_comms.py

--- Mainboard Communication Tasks/BTS_test_robot_basic_comms.py
+++ Mainboard Communication Tasks/BTS_test_robot_basic_comms.py
@@ -9,10 +9,9 @@ def send_ms(ser,speeds): #unpack motorspeeds
 
 def send_motorspeeds(ser,m1 = 0,m2 = 0,m3 = 0,thrower = 0): # send speeds to motors and return data
     write_this = struct.pack('<hhhHBH', m1, m2, m3, thrower, disable_failsafe, 0xAAAA)
-    print(write_this)
+    #print(write_this)
     ser.write(write_this)
     size = struct.calcsize('<hhhH')
-    #print(size)
     buffer_size = ser.read(size)
     #buffer_size = ser.readline()
     values = struct.unpack('<hhhH',buffer_size)
@@ -21,10 +20,10 @@ def send_motorspeeds(ser,m1 = 0,m2 = 0,m3 = 0,thrower = 0): # send speeds to mot
 count = 0
 
 disable_failsafe = 0
-speed1 = 10
-speed2 = 0
-speed3 = -10
-thrower_speed = 100
+speed1 = 15
+speed2 = 5
+speed3 = -15
+thrower_speed = 0
 
 speeds = [speed1, speed2, speed3, thrower_speed]
 
@@ -32,16 +31,16 @@ ser = None
 try:
 	port='/dev/ttyACM1'
 	ser = serial.Serial(port, baudrate=115200, timeout=3)
-	print('send / receive data loop started...')
-	while True:
-	#while count < 20:
-		#ser.write()
-		#motor_data = send_ms(ser,speeds)
-		send_ms(ser,speeds)
-		#print(motor_data)
-		#sleep(1.05)
+	#while True:
+	print('Loop started!')
+	while count < 10000: # resulting motion at least 1m?
+		motor_data = send_ms(ser,speeds)
+		#send_ms(ser,speeds)
+		print(motor_data)
+		sleep(1.05)
 		count += 1
-		print('loop iteration =  ',count)
+		#print('loop iteration =  ',count)
+	
 	print('Loop finished!')
 except Exception as e:
 	print(e)
